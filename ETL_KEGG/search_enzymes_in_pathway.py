d92ab29b0888144f79237b2b4793faf1ae38083e
dMedinaO <41024522+dMedinaO@users.noreply.github.com>
2022-05-25 07:16:50 -0400

    Adding change log error

--- ETL_KEGG/search_enzymes_in_pathway.py
+++ ETL_KEGG/search_enzymes_in_pathway.py
@@ -33,7 +33,6 @@ for i in range(len(dataset_pathways)):
     #lista de genes identificados
     for value in values_data:
         data = value.split("[")[-1].replace("]", "").split(" ")
-        
         for element in data:
             if "EC" not in element:
                 element = "EC:{}".format(element)
@@ -47,5 +46,7 @@ print(len(matrix_enzyme))
 
 print("Processing data to export")
 matrix_to_export = [value.split(";") for value in matrix_enzyme]
-df_export = pd.DataFrame(matrix_to_export, columns=['organism', 'pathway', 'code_enzyme'])
+ok_data = [value for value in matrix_to_export if len(value) == 3]
+
+df_export = pd.DataFrame(ok_data, columns=['organism', 'pathway', 'code_enzyme'])
 df_export.to_csv(name_export, index=False)
--- ETL_KEGG/search_patways.py
+++ ETL_KEGG/search_patways.py
@@ -6,21 +6,31 @@ dataset = pd.read_csv(sys.argv[1])
 name_export = sys.argv[2]
 
 matrix_pathways = []
+code_error_list = []
 
 for i in range(len(dataset)):
     organism = dataset['code'][i]
     print("Processing organism: ", organism)
-    pathways = REST.kegg_list("pathway", organism).read()
 
-    pathways = pathways.split("\n")
+    try:
+        pathways = REST.kegg_list("pathway", organism).read()
 
-    for element in pathways:
-        if len(element)>1:
-            element = element.split("\t")
-            values = [value.strip() for value in element]
-            values.insert(0, organism)
-            matrix_pathways.append(values)
-    
+        pathways = pathways.split("\n")
+
+        for element in pathways:
+            if len(element)>1:
+                element = element.split("\t")
+                values = [value.strip() for value in element]
+                values.insert(0, organism)
+                matrix_pathways.append(values)
+    except:
+        code_error_list.append(organism)
+    break
+
 print("Exporting data")
 df_export = pd.DataFrame(matrix_pathways, columns=['organism', 'path_id', 'description_path'])
 df_export.to_csv(name_export, index=False)
+
+df_error = pd.DataFrame()
+df_error['organism_error'] = code_error_list
+df_error.to_csv("error_list_organism_found_pathwats.csv", index=False)
